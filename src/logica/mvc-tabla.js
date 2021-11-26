e9d712f12a27078fe97f6e6cf4884e54e52f82ef
Lorena <rainbowg03@gmail.com>
2021-11-26 11:06:57 +0100

    informes registros 26/11/2021
    Acabado informa de descalibrados desde hace 4 horas, y corregidos fallos de los informes

--- src/logica/mvc-tabla.js
+++ src/logica/mvc-tabla.js
@@ -68,37 +68,43 @@ var ControladorTabla = {
     }
 }
 
-function createPDF() {
+function createPDFAveriados() {
     var tabla = document.getElementById('tblDatos');
     let tablaM ="<table><tbody>";
     var estado = tabla.getElementsByClassName("Averiado");
     var fecha = tabla.getElementsByClassName("Fecha");
+
       for (let i = 0; i < tabla.rows.length; i++) {
-        tablaM += "<tr>"; 
-        averiado = estado[i]
-        fechaDeUno = fecha[i]
+        tablaM += "<tr>";
         
-        if(averiado!=undefined && averiado.innerHTML=="1"){
-            let fechaDate = new Date(fechaDeUno.innerText)
-            //fechaDeUno = new Date(fechaDeUno.innerHTML)
-            let horas = (Date.now() - fechaDate.getTime())/1000/60/24
-            if(horas>=24){
-                for (let j = 0; j < tabla.rows[i].cells.length; j++) {
-                    let texto = ""
-                   
-                    
-                    texto = tabla.rows[i].cells[j].innerText
-                    if(i == 0 && !texto.includes("Leído")) {
-                      console.log(texto);
-                      tablaM += "<th>" + tabla.rows[i].cells[j].innerHTML + "</th>"
-        
-                    } else if (!texto.includes("Marcar como leído") && !texto.includes("Leído")) {
-                      tablaM += "<td>" + tabla.rows[i].cells[j].innerHTML + "</td>"
-                      
-                    } 
-                                
-                }
+        if (i==0) {
+            for(let j = 0; j<tabla.rows[i].cells.length-1; j++){
+                tablaM += "<th>" + tabla.rows[i].cells[j].innerHTML + "</th>"
             }
+        } else {
+            
+            averiado = estado[i-1]
+            fechaDeUno = fecha[i-1]
+            if(averiado != undefined && averiado.innerHTML=="1"){
+                let fechaDate = new Date(fechaDeUno.innerText)
+                let horas = (Date.now() - fechaDate.getTime())/1000/60/24
+                
+                
+                
+                if( i> 0 && horas>=24){
+                    console.log("la i", i)
+                    for (let j = 0; j < tabla.rows[i].cells.length-1; j++) {
+                        let texto = ""                   
+                        texto = tabla.rows[i].cells[j].innerText
+                        
+                        console.log("", texto);
+                        tablaM += "<td>" + tabla.rows[i].cells[j].innerHTML + "</td>"            
+                    }
+                }
+            
+        }
+
+        
         }
         tablaM += "</tr>";
       }
@@ -117,6 +123,70 @@ function createPDF() {
     win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
     win.document.write('</head><body>');
     win.document.write('<h1>Sensores averiados de más de 24 horas</h1>');
+    win.document.write(tablaM);       // THE TABLE CONTENTS INSIDE THE BODY TAG.
+    win.document.write('</body></html>');
+
+    win.document.close(); 	// CLOSE THE CURRENT WINDOW.
+
+    win.print();    // PRINT THE CONTENTS.
+}
+
+
+function createPDFDescalibrados() {
+    var tabla = document.getElementById('tblDatos');
+    let tablaM ="<table><tbody>";
+    var estado = tabla.getElementsByClassName("Averiado");
+    var fecha = tabla.getElementsByClassName("Fecha");
+
+      for (let i = 0; i < tabla.rows.length; i++) {
+        tablaM += "<tr>";
+        
+        if (i==0) {
+            for(let j = 0; j<tabla.rows[i].cells.length-1; j++){
+                tablaM += "<th>" + tabla.rows[i].cells[j].innerHTML + "</th>"
+            }
+        } else {
+            
+            descalibrado = estado[i-1]
+            fechaDeUno = fecha[i-1]
+            if(descalibrado != undefined && descalibrado.innerHTML=="1"){
+                let fechaDate = new Date(fechaDeUno.innerText)
+                let horas = (Date.now() - fechaDate.getTime())/1000/60/24
+                
+                
+                
+                if( i> 0 && horas>=4){
+                    console.log("la i", i)
+                    for (let j = 0; j < tabla.rows[i].cells.length-1; j++) {
+                        let texto = ""                   
+                        texto = tabla.rows[i].cells[j].innerText
+                        
+                        console.log("", texto);
+                        tablaM += "<td>" + tabla.rows[i].cells[j].innerHTML + "</td>"            
+                    }
+                }
+            
+        }
+
+        
+        }
+        tablaM += "</tr>";
+      }
+      tablaM += "</tbody></table>";
+
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 17px Calibri;}";
+    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "</style>";
+
+    // CREATE A WINDOW OBJECT.
+    var win = window.open('', '', 'height=700,width=700');
+
+    win.document.write('<html><head>');
+    win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
+    win.document.write('</head><body>');
+    win.document.write('<h1>Sensores descalibrados de más de 4 horas</h1>');
     win.document.write(tablaM);       // THE TABLE CONTENTS INSIDE THE BODY TAG.
     win.document.write('</body></html>');
 
